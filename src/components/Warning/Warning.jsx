0625a32f79e3f417df04468f90c63d8a0321d78d
Maxim Marchuk <maxim.marchuk.d.corp@gmail.com>
2022-03-31 02:43:13 +0300

    Fix a warning message

--- src/components/Warning/Warning.jsx
+++ src/components/Warning/Warning.jsx
@@ -7,40 +7,50 @@ import './styles.scss';
 const Warning = (props) => {
   const { message, isVisible, hideWarning } = props;
 
+  const initialIntervalId = null;
   const initialCounter = 10;
 
+  const [intervalId, setIntervalId] = useState(initialIntervalId);
   const [counter, setCounter] = useState(initialCounter);
 
   useEffect(() => {
     if (isVisible) {
       startCounter();
+    } else {
+      resetCounter();
     }
   }, [isVisible]);
 
   const startCounter = () => {
     const delay = 1000; // 1 sec
-    const deadline = 0;
+    const deadline = 0; // stop in 0 sec
     const step = -1;
 
     let nextCounter = initialCounter;
 
-    const intervalId = setInterval(() => {
+    const nextIntervalId = setInterval(() => {
       nextCounter = nextCounter + step;
 
       setCounter(nextCounter);
 
       if (nextCounter === deadline) {
         hideWarning();
-
-        resetCounter();
-
-        clearInterval(intervalId);
       }
     }, delay);
+
+    setIntervalId(nextIntervalId);
   };
 
   const resetCounter = () => {
     setCounter(initialCounter);
+
+    resetIntervalId();
+  };
+
+  const resetIntervalId = () => {
+    clearInterval(intervalId);
+
+    setIntervalId(initialIntervalId);
   };
 
   return isVisible ? (
